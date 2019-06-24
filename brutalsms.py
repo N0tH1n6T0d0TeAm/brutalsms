cc080adf40a594d843b5f59335d1d44b35282d81
N0tH1n6T0d0TeAm <51526066+N0tH1n6T0d0TeAm@users.noreply.github.com>
2019-06-24 12:55:35 +0800

    brutalsms.py

--- brutalsms.py
+++ brutalsms.py
@@ -1,5 +1,2 @@
-import os
-os.system("mkdir /$HOME/NjankSoekamtiHERE")
-os.system("xdg-open https://youtube.com/NjankSoekamti")
-os.system("clear")
-print("\n\n Saya tutup sampe lebaran!\n\n")
+NOOOOOOOOOOOB
+;
